2662dbca3c50ff7effbe3a97205730ab9d46b65e
Amit Gupta <amit@gprof.com>
2021-03-14 18:31:23 -0700

    Added Google Sheet integration

--- PhotoText001/CameraViewController.swift
+++ PhotoText001/CameraViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 import Vision
 import VisionKit
 
-class ViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
+class CameraViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
+    
+    var co2level=0;
+    var tempReading=0.0;
+    var humidityReading=0;
+    var manufacturer="Unknown";
+    let co2limit=250; // Number above is CO2 ppm
+    let humidityLimit=5; // Ignore numbers below
     
     var textRecognitionRequest = VNRecognizeTextRequest(completionHandler: nil)
     private let textRecognitionWorkQueue = DispatchQueue(label: "MyVisionScannerQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
@@ -31,6 +38,41 @@ class ViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
         present(scannerViewController, animated: true)
     }
     
+    func initReading() {
+        co2level=0;
+        tempReading=0;
+        humidityReading=0;
+        manufacturer="Unknown"
+    }
+    
+    func createReading() -> String {
+        let s=String(format:"CO2 %d Temp %.1f Humidity %d",co2level,tempReading,humidityReading)
+        return s;
+    }
+    
+    func processString(_ s:String) {
+        let cs = CharacterSet.init(charactersIn: "01234567890 .").inverted
+        let results = s.components(separatedBy: cs)
+        print("Processing string:",s);
+        for i in results {
+            //print("Saw:",i," count",i.count);
+            if(i.count<=1) {
+                continue
+            }
+            if(i.contains(".")) { // Floating point - Temperature
+                tempReading=Double(i) ?? 0.0
+            } else {
+                let j=Int(i) ?? 0
+                if(j>=co2limit) {
+                    co2level = j
+                } else if(j>=humidityLimit){
+                    humidityReading = j
+                }
+                
+        };
+    }
+    }
+    
     private func setupVision() {
         textRecognitionRequest = VNRecognizeTextRequest { (request, error) in
             guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
@@ -38,13 +80,18 @@ class ViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
             var detectedText = ""
             for observation in observations {
                 guard let topCandidate = observation.topCandidates(1).first else { return }
-                print("text \(topCandidate.string) has confidence \(topCandidate.confidence)")
+                //print("text \(topCandidate.string) has confidence \(topCandidate.confidence)");
+                self.processString(topCandidate.string);
     
-                detectedText += topCandidate.string
-                detectedText += "\n"
+                detectedText += topCandidate.string;
+                detectedText += "\n";
                 
             
             }
+            let s=self.createReading()
+            print("Final reading:",s)
+            let cdt = self.getCurrentDateTime()
+            GoogleSheetsIntegration.recordSensor(source:"Photo",co2:self.co2level,timestamp:cdt, tempF:Float(self.tempReading), pressure:0, humidity:self.humidityReading,battery:0)
             
             DispatchQueue.main.async {
                 self.textView.text = detectedText
@@ -55,6 +102,15 @@ class ViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
 
         textRecognitionRequest.recognitionLevel = .accurate
     }
+    
+    /*
+     text 72.5°F 34 ® has confidence 1.0
+     text 628 has confidence 1.0
+     text O has confidence 0.5
+     text ppm COz has confidence 0.5
+     text * has confidence 0.5
+     text aranet4 has confidence 0.5
+     */
     
     private func processImage(_ image: UIImage) {
         imageView.image = image
@@ -104,5 +160,16 @@ class ViewController: UIViewController, VNDocumentCameraViewControllerDelegate {
         }
         return reloadedImage
     }
+    
+    func getCurrentDateTime() -> String {
+           let currentDateTime=Date()
+           let formatter=DateFormatter()
+           formatter.timeStyle = .medium
+           formatter.dateStyle = .none
+           let dateTimeStr=formatter.string(from: currentDateTime)
+           return dateTimeStr
+       }
 }
+
+
 
